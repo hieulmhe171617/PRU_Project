cf9b5bb21187b1f815b2c92c13e9de4c191ab77d
Huuloc <ngohuuloc54@gmail.com>
2024-07-06 09:59:49 +0700

    Merge phase 1

--- Assets/Scripts/Core/NextLevel.cs
+++ Assets/Scripts/Core/NextLevel.cs
@@ -2,18 +2,19 @@ using UnityEngine;
 
 public class NextLevel : MonoBehaviour
 {
-    private LoadingManager loadingManager;
+    //private LoadingManager loadingManager;
 
     private void Awake()
     {
-        loadingManager = FindFirstObjectByType<LoadingManager>();
+        //loadingManager = FindFirstObjectByType<LoadingManager>();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            loadingManager.GoToNextScene();
+            //Debug.Log("LoadingManager: "+loadingManager);
+            LoadingManager.Ins.GoToNextScene();
         }
     }
 
--- Assets/Scripts/LoadingManager.cs
+++ Assets/Scripts/LoadingManager.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LoadingManager : MonoBehaviour
+public class LoadingManager : Singleton<LoadingManager>
 {
     int sceneIndex;
     int lastScenceIndex;
